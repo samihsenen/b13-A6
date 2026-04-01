9f03d2b434713b6bb3a48198617aadfa5e5c3736
samihsenen <samihsenen@gmail.com>
2026-04-01 19:32:55 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+ <div className="navbar bg-base-100 shadow-sm">
+  <a className="btn btn-ghost text-xl">daisyUI</a>
+</div>
+    </>
+  )
+}
+
+export default App
